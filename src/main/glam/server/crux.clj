2524d4ae6d470531e72a149aae926d97f2bf7ba6
Luke Gessler <lukegessler@gmail.com>
2021-05-31 16:38:01 -0400

    some pointers on websocket sync

--- src/main/glam/server/crux.clj
+++ src/main/glam/server/crux.clj
@@ -4,7 +4,8 @@
             [glam.crux.easy :refer [install-tx-fns!]]
             [glam.server.config :refer [config]]
             [clojure.java.io :as io])
-  (:import [crux.api ICruxAPI]))
+  (:import [crux.api ICruxAPI]
+           (java.io StringWriter)))
 
 (defn ^ICruxAPI start-lmdb-node [{:keys [db-dir http-server-port]}]
   (let [dirf #(.getPath (io/file db-dir %))]
@@ -15,15 +16,39 @@
           (cond-> http-server-port (assoc :crux.http-server/server {:port http-server-port}))))))
 
 (defn start-main-lmdb-node []
-  (start-lmdb-node {:db-dir (-> config ::config :main-db-dir)}))
+  (start-lmdb-node {:db-dir           (-> config ::config :main-db-dir)
+                    :http-server-port (-> config ::config :http-server-port)}))
 
 (defn start-session-lmdb-node []
-  (start-lmdb-node {:db-dir           (-> config ::config :session-db-dir)
-                    :http-server-port (-> config ::config :http-server-port)}))
+  (start-lmdb-node {:db-dir (-> config ::config :session-db-dir)}))
+
+(defn extract-ids
+  [seq]
+  )
+
+(defn setup-listener! [node]
+  (crux/listen
+    node
+    {:crux/event-type :crux/indexed-tx, :with-tx-ops? true}
+    (fn [ev]
+      (let [out (StringWriter.)]
+        (clojure.pprint/pprint ev out)
+        (println
+          (str
+            "\nevent received!\n\n"
+            (.toString out)
+            "\n\n\n")))
+      (when (:committed? ev)
+        (println (-> ev :crux/tx-ops first second type))
+        (println (-> ev :crux/tx-ops first second))
+
+        ))))
 
 (defstate crux-node
   :start (let [node (start-main-lmdb-node)]
            (install-tx-fns! node)
+           ;; TODO: keep going with this for websocket sync
+           ;; (setup-listener! node)
            node)
   :stop (.close crux-node))
 
--- src/main/glam/server/server.clj
+++ src/main/glam/server/server.clj
@@ -3,9 +3,16 @@
     [clojure.pprint :refer [pprint]]
     [io.pedestal.http :as http]
     [mount.core :refer [defstate]]
+    [com.fulcrologic.fulcro.server.api-middleware :refer [not-found-handler]]
+    [com.fulcrologic.fulcro.networking.websockets :as fws]
     [glam.server.config :refer [config]]
     [glam.server.service :as service]
     [taoensso.timbre :as log]))
+
+;; https://github.com/ptaoussanis/sente/blob/master/src/taoensso/sente/server_adapters/jetty9.clj
+;; https://github.com/pedestal/pedestal/blob/master/jetty/src/io/pedestal/http/jetty/websockets.clj
+;; https://github.com/pedestal/pedestal/blob/master/samples/jetty-web-sockets/src/jetty_web_sockets/service.clj
+;; https://github.com/fulcrologic/fulcro-websockets/blob/develop/src/main/com/fulcrologic/fulcro/networking/websocket_remote.cljc
 
 (defstate http-server
   :start
