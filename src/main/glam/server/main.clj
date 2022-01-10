024573a1b62850af06fc33c87bbd3f503b6bc66a
Luke Gessler <lukegessler@gmail.com>
2022-01-10 12:59:58 -0500

    remove code from docs branch

--- src/main/glam/server/main.clj
+++
@@ -1,9 +0,0 @@
-(ns glam.server.main
-  (:require
-    [mount.core :as mount]
-    glam.server.http-server)
-  (:gen-class))
-
-(defn -main [& args]
-  (println "args: " args)
-  (mount/start-with-args {:config "config/prod.edn"}))
