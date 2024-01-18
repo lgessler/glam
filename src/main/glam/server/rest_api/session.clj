8d2d9aa7b8038d781f565c464bf3e99b439175c7
Luke Gessler <lukegessler@gmail.com>
2024-01-18 16:31:59 -0700

    impl span endpoints

--- src/main/glam/server/rest_api/session.clj
+++ src/main/glam/server/rest_api/session.clj
@@ -7,14 +7,14 @@
         {valid? :session/valid? message :server/message :as session-updates} (`gms/login result)
         new-session (merge (:session req) session-updates)]
     (if valid?
-      {:status 200 :body {:message "User logged in."} :session new-session}
-      {:status 401 :body {:message message}})))
+      {:status 200 :body {:message "User logged in." :error false} :session new-session}
+      {:status 401 :body {:message message :error true}})))
 
 (defn logout [req]
   (let [result (parser req [(list `gms/logout {})])
         session-updates (`gms/logout result)
         new-session (merge (:session req) session-updates)]
-    {:status 200 :body {:message "User logged out."} :session new-session}))
+    {:status 200 :body {:message "User logged out." :error false} :session new-session}))
 
 (defn register [{{{:keys [username password]} :body} :parameters :as req}]
   (let [result (parser req [(list `gms/signup {:username username :password password})])
@@ -22,8 +22,8 @@
         {valid? :session/valid? message :session/server-error-msg :as session-updates} (`gms/signup result)
         new-session (merge (:session req) session-updates)]
     (if valid?
-      {:status 200 :body {:message "User created and logged in."} :session new-session}
-      {:status 401 :body {:message message}})))
+      {:status 200 :body {:message "User created and logged in." :error false} :session new-session}
+      {:status 401 :body {:message message :error true}})))
 
 (def session-routes
   ["/session"
