0ad2676fa356419b66493f84177921b35d2f1139
Luke Gessler <lukegessler@gmail.com>
2024-01-19 10:41:43 -0700

    impl update-tokens

--- src/main/glam/server/rest_api/span.clj
+++ src/main/glam/server/rest_api/span.clj
@@ -2,7 +2,8 @@
   (:require [glam.server.pathom-parser :refer [parser]]
             [glam.server.id-counter :refer [id?]]
             [glam.models.span :as span]
-            [glam.server.rest-api.util :as util]))
+            [glam.server.rest-api.util :as util]
+            [malli.experimental.lite :as ml]))
 
 (defn create-span [{{{:keys [value layer tokens]} :body} :parameters :as req}]
   (let [result (parser req [(list `span/create-span {:span/layer layer :span/value value :span/tokens tokens})])
@@ -20,25 +21,39 @@
      :body   data}))
 
 (defn patch-span [{{{:keys [id]} :path
-                    {:keys [action value]} :body} :parameters :as req}]
-  (let [result (parser req [(list `span/save-span {:span/id id :span/value value})])
-        data (get result `span/save-span)]
-    (if (:server/error? data)
-      {:status 403
-       :body   data}
-      {:status 200
-       :body   data})))
+                    {:keys [action value tokens]} :body} :parameters :as req}]
+  (let [action-symbol ({"setValue" `span/update-value
+                        "setTokens" `span/update-tokens} action)
+        action-params ({"setValue" {:span/value value}
+                        "setTokens" {:span/tokens tokens}} action)]
+    (if (nil? action-symbol)
+      {:status 400
+       :body {:error true :message (str "Unknown action: `" action "`")}}
+      (let [result (parser req [(list action-symbol (merge {:span/id id} action-params))])
+            data (get result action-symbol)]
+        (if (:server/error? data)
+          {:status 400
+           :body   data}
+          {:status 200
+           :body   data})))))
 
 (def span-routes
   ["/span"
    [""
-    {:post {:parameters {:body {:value any? :layer id? :tokens [:vector id?]}}
+    {:post {:parameters {:body {:value any?
+                                :layer id?
+                                :tokens [:vector id?]}}
+            :description "Creates a new span. ID is given in the response under \"id\"."
             :handler    create-span}}]
 
    ["/:id"
     {:get {:parameters {:path {:id id?}}
            :handler    get-span}
-     :patch {:parameters {:path {:id id?}
-                          :body {:action [:enum "setValue"] :value any?}}
-             :description "setValue: sets the span's `value` to body param `value`."
-             :handler patch-span}}]])
+     :patch
+     {:parameters {:path {:id id?}
+                   :body {:action [:enum "setValue" "setTokens"]
+                          :value  (ml/optional any?)
+                          :tokens (ml/optional [:vector id?])}}
+      :description "setValue: sets the span's `value` to body param `value`.
+                    setTokens: overwrite the span's `tokens` so that it exactly matches what's in body param `tokens`"
+      :handler patch-span}}]])
